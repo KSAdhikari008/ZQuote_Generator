01d0b67a58ebb37edaa291a5af4a0c2ea336eec4
Adhikari <adhikarifeb2001@gmail.com>
2026-09-19 19:53:28 +0530

    style: body and header sytling

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,14 @@ import './App.css'
 function App() {
  
 
- 
   return (
     <div className="app">
       <header>
         <div className="left-section">
-          <div className="brand">ZQUOTELY</div>
+          <div className="brand">QUOTELY</div>
         </div>
         <div className="right-section">
-          <div className="theme-toggle"></div>
+          <div className="theme-toggle">theme</div>
         </div>
       </header>
       <main>
