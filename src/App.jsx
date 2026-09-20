697d6990176b56decce41e7ba6def6e634efe655
Adhikari <adhikarifeb2001@gmail.com>
2026-09-20 13:41:41 +0530

    feat: fetch quote using api

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
+import { useState } from 'react';
 import './App.css'
 
 function App() {
- 
+
+  const [quote, setQuote] = useState('');
+
+  async function getQuote(){
+
+    const URL = import.meta.env.VITE_API_URL;
+    const API_KEY = import.meta.env.VITE_API_KEY;
+    
+    const res = await fetch(
+      `${URL}/v2/randomquotes?categories=wisdom`,
+      {
+        headers: {
+          "X-Api-Key": API_KEY,
+        },
+      }
+    );
+    const data  = await res.json();
+
+    console.log(data);
+    setQuote(data[0].quote);
+  }
 
   return (
     <div className="app">
@@ -14,9 +35,11 @@ function App() {
         </div>
       </header>
       <main>
-        <div className="quote-container"></div>
+        <div className="quote-container">
+          {quote && quote}
+        </div>
         <div className="tags">#life</div>
-        <button className="heading">Random Quote</button>
+        <button className="heading" onClick={getQuote}>Get Random Quote</button>
       </main>
     </div>
   );
