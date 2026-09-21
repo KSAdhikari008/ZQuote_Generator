a6c6be4d495629434b22e96b399ca5b1ef634146
Adhikari <adhikarifeb2001@gmail.com>
2026-09-21 20:22:47 +0530

    feat: impliment loading state

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,33 @@ import './App.css'
 function App() {
 
   const [quote, setQuote] = useState('');
-
+  const [loading, setLoading] = useState(false);
+  
   async function getQuote(){
+    setLoading(true);
 
-    const URL = import.meta.env.VITE_API_URL;
-    const API_KEY = import.meta.env.VITE_API_KEY;
-    
-    const res = await fetch(
-      `${URL}/v2/randomquotes?categories=wisdom`,
-      {
-        headers: {
-          "X-Api-Key": API_KEY,
-        },
-      }
-    );
-    const data  = await res.json();
+    try{
+      
+      const URL = import.meta.env.VITE_API_URL;
+      const API_KEY = import.meta.env.VITE_API_KEY;
+      
+      
+      const res = await fetch(
+        `${URL}/v2/randomquotes?categories=wisdom`,
+        {
+          headers: {
+            "X-Api-Key": API_KEY,
+          },
+        }
+      );
+      const data  = await res.json();
+      setQuote(data[0]);
 
-    console.log(data);
-    setQuote(data[0]);
+    }catch(err){
+      console.error(err);
+    }finally{
+      setLoading(false);
+    }
   }
 
   return (
@@ -37,20 +46,26 @@ function App() {
 
       <main>
         <input type='text'  className="search-category"/>
-        {quote && 
-        <div className="quote-container">
-          <div className="quote">
-            {quote.quote}
+        {loading 
+          ? <div className="dots">
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
+          : quote && 
+          <div className="quote-container">
+            <div className="quote">
+              {quote.quote}
+            </div>
+            <div className="author">
+              - {quote.author ? quote.author : "unknown"}
+            </div>
+            <div className="categories">
+              {quote.categories.map(c => "#"+c+" ")}
+            </div>
           </div>
-          <div className="author">
-            - {quote.author ? quote.author : "unknown"}
-          </div>
-          <div className="categories">
-            {quote.categories.map(c => "#"+c+" ")}
-          </div>
-        </div>
         }
-        <button className="getBtn" onClick={getQuote}>Get Quote</button>
+        <button className="getBtn" onClick={getQuote} disabled={loading && true}>Get Quote</button>
       </main>
     </div>
   );
